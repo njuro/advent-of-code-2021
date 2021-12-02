325acc208750674d25069beb7009acf0c9e821bd
njuro <juraj.noge@gmail.com>
2021-12-02 08:08:11 +0100

    Day 1 - functional solution

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -3,13 +3,9 @@ import utils.readInputLines
 /** [https://adventofcode.com/2021/day/1] */
 class Depths : AdventOfCodeTask {
     override fun run(part2: Boolean): Any {
-        val depths = readInputLines("1.txt").map(String::toInt)
-
-        return if (part2)
-            depths.windowed(size = 3, step = 1, partialWindows = false)
-                .zipWithNext()
-                .count { it.second.sum() > it.first.sum() }
-        else depths.zipWithNext().count { it.second > it.first }
+        return readInputLines("1.txt").map(String::toInt).let {
+            if (part2) it.windowed(3) { it.sum() } else it
+        }.zipWithNext().count { it.second > it.first }
     }
 }
 
