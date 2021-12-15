875c879d231f357d81b4a0ec39303d82dfe4d57c
njuro <juraj.noge@gmail.com>
2021-12-15 08:56:02 +0100

    Day 15 - part 2

--- src/main/kotlin/day15.kt
+++ src/main/kotlin/day15.kt
@@ -1,38 +1,50 @@
 import utils.Coordinate
 import utils.readInputLines
+import java.util.*
 
 /** [https://adventofcode.com/2021/day/15] */
 class Day15 : AdventOfCodeTask {
     override fun run(part2: Boolean): Any {
-        val chitons = readInputLines("15.txt").flatMapIndexed { y, row ->
+        val original = readInputLines("15.txt").flatMapIndexed { y, row ->
             row.mapIndexed { x, c -> Coordinate(x, y) to c.digitToInt() }
-        }.toMap().toMutableMap().withDefault { -1 }
+        }.toMap()
+        val width = original.keys.maxOf { it.x } + 1
+        val height = original.keys.maxOf { it.y } + 1
+
+        fun Int.increase(value: Int) = (this + value).let { if (it > 9) (it % 10) + 1 else it }
+        fun Map<Coordinate, Int>.enlarged() =
+            toMutableMap().apply {
+                putAll((1..4).flatMap {
+                    map { (coordinate, value) ->
+                        Coordinate(coordinate.x + it * width, coordinate.y) to value.increase(it)
+                    }
+                })
+                putAll((1..4).flatMap {
+                    map { (coordinate, value) ->
+                        Coordinate(coordinate.x, coordinate.y + it * height) to value.increase(it)
+                    }
+                })
+            }
+
+        val map = if (part2) original.enlarged() else original
         val start = Coordinate(0, 0)
-        val end = Coordinate(chitons.keys.maxOf { it.x }, chitons.keys.maxOf { it.y })
+        val end = Coordinate(map.keys.maxOf { it.x }, map.keys.maxOf { it.y })
 
-        val unvisited = chitons.keys.toMutableSet()
-        val distances = chitons.keys.associateWith { Int.MAX_VALUE }.toMutableMap()
-        distances[start] = 0
+        val queue = PriorityQueue<Pair<Coordinate, Int>>(Comparator.comparing { it.second }).apply { add(start to 0) }
+        val seen = mutableSetOf<Coordinate>()
+        while (queue.isNotEmpty() && queue.peek().first != end) {
+            val (position, cost) = queue.poll()
+            position.adjacent().values.filter { it in map && it !in seen }.forEach { neighbour ->
+                seen.add(neighbour)
+                queue.add(neighbour to cost + map.getValue(neighbour))
+            }
 
-        var current = start
-        while (unvisited.isNotEmpty()) {
-            current.adjacent().values.filter { it in unvisited }.forEach { neighbour ->
-                val newDistance = distances.getValue(current) + chitons.getValue(neighbour)
-                if (newDistance < distances.getValue(neighbour)) {
-                    distances[neighbour] = newDistance
-                }
-            }
-            if (current == end) {
-                break
-            }
-            unvisited.remove(current)
-            current = unvisited.minByOrNull { distances.getValue(it) }!!
         }
 
-        return distances.getValue(end)
+        return queue.poll().second
     }
 }
 
 fun main() {
-    print(Day15().run(part2 = false))
+    print(Day15().run(part2 = true))
 }
