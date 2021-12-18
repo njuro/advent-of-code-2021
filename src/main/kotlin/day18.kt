7ec1babc3fcbf5bf1c842cfbef0f8a339e02830c
njuro <juraj.noge@gmail.com>
2021-12-18 09:31:57 +0100

    Day 18 - cmooon

--- src/main/kotlin/day18.kt
+++ src/main/kotlin/day18.kt
@@ -67,42 +67,91 @@ class Day18 : AdventOfCodeTask {
             }
         }
 
-        var parent: SnailFish? = null
+        var parent: SnailPair? = null
 
-        abstract fun populate(newParent: SnailFish?)
+        abstract fun populate(newParent: SnailPair?)
 
-        abstract fun reduced(level: Int = 1): Pair<SnailFish, Boolean>
+        abstract fun reduced(level: Int): Pair<SnailFish, Boolean>
+
+        abstract fun explodeLeft(value: Int)
+
+        abstract fun explodeRight(value: Int)
 
         abstract fun magnitude(): Long
 
-        fun add(other: SnailFish) = SnailPair(left = this, right = other).also { it.populate(null) }.reduced().first
+        fun add(other: SnailFish) = SnailPair(left = this, right = other).also { it.populate(null) }.reduced()
 
 
+        fun reduced(): SnailFish {
+            var current = this
+            while (true) {
+                println(current)
+                val (newValue, wasReduced) = current.reduced(0).also { it.first.populate(null) }
+                if (!wasReduced) {
+                    break
+                }
+                println(newValue)
+                println()
+                current = newValue
+            }
+            return current
+        }
     }
 
     data class SnailPair(var left: SnailFish, var right: SnailFish) : SnailFish() {
         override fun reduced(level: Int): Pair<SnailFish, Boolean> {
-            var current = this
-            while (true) {
-                if (level == 4) {
-
-                    var newParent = parent
-                    return SnailLiteral(0).apply { this.parent = newParent } to true
-                } else {
-                    var newLeft = left.reduced(level + 1)
-                    var newRight = right.reduced(level + 1)
-                    var newParent = parent
-                    current = SnailPair(newLeft.first, newRight.first).apply { this.parent = newParent }
-                    if (!newLeft.second && !newRight.second) {
-                        break
-                    }
+            if (level == 4) {
+                println("EXPLODE $this")
+                explodeLeft((this.left as SnailLiteral).value)
+                explodeRight((this.right as SnailLiteral).value)
+                var newParent = parent
+                return SnailLiteral(0).apply { this.parent = newParent } to true
+            } else {
+                var newParent = parent
+                var newLeft = left.reduced(level + 1)
+                if (newLeft.second) {
+                    return SnailPair(newLeft.first, right).apply { this.parent = newParent } to true
                 }
-            }
+                var newRight = right.reduced(level + 1)
+                if (newRight.second) {
+                    return SnailPair(left, newRight.first).apply { this.parent = newParent } to true
+                }
 
-            return current to false
+                return this to false
+            }
         }
 
-        override fun populate(newParent: SnailFish?) {
+        override fun explodeLeft(value: Int) {
+            val current = this.parent ?: return
+            if (current.left === this) {
+                current.explodeLeft(value)
+                return
+            }
+            if (current.left is SnailPair) {
+                var ok: SnailFish = (current.left as SnailPair).right
+                while (ok is SnailPair) ok = ok.right
+                ok.explodeRight(value)
+                return
+            }
+            current.left.explodeRight(value)
+        }
+
+        override fun explodeRight(value: Int) {
+            val current = this.parent ?: return
+            if (current.right === this) {
+                current.explodeRight(value)
+                return
+            }
+            if (current.right is SnailPair) {
+                var ok: SnailFish = (current.right as SnailPair).left
+                while (ok is SnailPair) ok = ok.left
+                ok.explodeLeft(value)
+                return
+            }
+            current.right.explodeLeft(value)
+        }
+
+        override fun populate(newParent: SnailPair?) {
             this.parent = newParent
             left.populate(this)
             right.populate(this)
@@ -111,11 +160,16 @@ class Day18 : AdventOfCodeTask {
         override fun magnitude(): Long {
             return 3 * left.magnitude() + 2 * right.magnitude()
         }
+
+        override fun toString(): String {
+            return "[$left,$right]"
+        }
     }
 
     data class SnailLiteral(var value: Int) : SnailFish() {
         override fun reduced(level: Int): Pair<SnailFish, Boolean> {
             if (value >= 10) {
+                println("SPLIT $this")
                 return SnailPair(
                     SnailLiteral(floor(value / 2.0).toInt()),
                     SnailLiteral(ceil(value / 2.0).toInt())
@@ -126,18 +180,30 @@ class Day18 : AdventOfCodeTask {
             return this to false
         }
 
-        override fun populate(newParent: SnailFish?) {
+        override fun explodeLeft(value: Int) {
+            this.value += value
+        }
+
+        override fun explodeRight(value: Int) {
+            this.value += value
+        }
+
+        override fun populate(newParent: SnailPair?) {
             this.parent = newParent
         }
 
         override fun magnitude(): Long {
             return value.toLong()
         }
+
+        override fun toString(): String {
+            return value.toString()
+        }
     }
 
     override fun run(part2: Boolean): Any {
         val input =
-            readInputLines("18.txt").map { SnailFish.parse(it.toList()).also { it.populate(null) }.reduced().first }
+            readInputLines("18.txt").map { SnailFish.parse(it.toList()).also { it.populate(null) }.reduced() }
 
         return input.reduce { acc, snailFish -> acc.add(snailFish) }
     }
