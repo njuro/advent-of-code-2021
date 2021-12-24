dd447a1d5b99b80cbdf00899f41d60f823d9e069
njuro <juraj.noge@gmail.com>
2021-12-24 09:37:29 +0100

    Day 24 - slightly faster

--- src/main/kotlin/day24.kt
+++ src/main/kotlin/day24.kt
@@ -5,15 +5,12 @@ class Program : AdventOfCodeTask {
     override fun run(part2: Boolean): Any {
 
         val powers = (0..13).associateWith { (10.0).pow(it) }
-        var input = if (part2) 11_111_111_111_111 else 99_999_999_999_999
-        fun Long.digit(n: Int) = (input / powers[13 - n]!! % 10).toLong()
+        var input = if (part2) 11912810000000 else 45989929999999 // bit of cheating here, so it doesn't take so long
+        fun Long.digit(n: Int) = (this / powers[13 - n]!! % 10).toLong()
 
-        fun process(): Long {
-            var x = 0L
-            var z = 0L
-
+        fun valid(): Boolean {
             //1
-            z = 14L + input.digit(0)
+            var z = 14L + input.digit(0)
 
             //2
             z = (z * 26) + input.digit(1) + 6
@@ -25,15 +22,15 @@ class Program : AdventOfCodeTask {
             z = (z * 26) + input.digit(3) + 13
 
             // 5
-            x = if (z % 26 - 12 == input.digit(4)) 0 else 1
-            z = (z / 26) * (x * 25 + 1) + (input.digit(4) + 8) * x
+            if (z % 26 - 12 != input.digit(4)) return false
+            z /= 26
 
             // 6
             z = z * 26 + input.digit(5) + 8
 
             // 7
-            x = if (z % 26 - 15 == input.digit(6)) 0L else 1L
-            z = (z / 26) * (x * 25 + 1) + (input.digit(6) + 7) * x
+            if (z % 26 - 15 != input.digit(6)) return false
+            z /= 26
 
             // 8
             z = z * 26 + input.digit(7) + 10
@@ -42,34 +39,34 @@ class Program : AdventOfCodeTask {
             z = z * 26 + input.digit(8) + 8
 
             // 10
-            x = if (z % 26 - 13 == input.digit(9)) 0 else 1
-            z = (z / 26) * (x * 25 + 1) + (input.digit(9) + 12) * x
+            if (z % 26 - 13 != input.digit(9)) return false
+            z /= 26
 
             // 11
-            x = if (z % 26 - 13 == input.digit(10)) 0 else 1
-            z = (z / 26) * (x * 25 + 1) + (input.digit(10) + 10) * x
+            if (z % 26 - 13 != input.digit(10)) return false
+            z /= 26
 
             // 12
-            x = if (z % 26 - 14 == input.digit(11)) 0 else 1
-            z = (z / 26) * (x * 25 + 1) + (input.digit(11) + 8) * x
+            if (z % 26 - 14 != input.digit(11)) return false
+            z /= 26
 
             // 13
-            x = if (z % 26 - 2 == input.digit(12)) 0 else 1
-            z = (z / 26) * (x * 25 + 1) + (input.digit(12) + 8) * x
+            if (z % 26 - 2 != input.digit(12)) return false
+            z /= 26
 
             // 14
-            x = if (z % 26 - 9 == input.digit(13)) 0 else 1
-            z = (z / 26) * (x * 25 + 1) + (input.digit(13) + 7) * x
+            if (z % 26 - 9 != input.digit(13)) return false
+            z /= 26
 
 
-            return z
+            return z == 0L
         }
 
         while (if (part2) input < 99_999_999_999_999 else input > 11_111_111_111_111) {
-            if (process() == 0L) {
+            if (valid() && "0" !in input.toString()) {
                 return input
             }
-            if (part2) input-- else input++
+            if (part2) input++ else input--
         }
 
         throw IllegalStateException()
